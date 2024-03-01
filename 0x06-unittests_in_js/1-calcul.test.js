385b8e1426ff0f39c2e0e73e8d421752010871b5
talent <124376251+CourageT1@users.noreply.github.com>
2024-03-01 13:49:28 +0000

    task1

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,27 @@
+// 1-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the subtraction of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" if the second number is 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
